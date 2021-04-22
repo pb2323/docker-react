3936d411a87eae82ae440e2894b84a9897056230
pb2323 <pbajaj0023@gmail.com>
2021-04-22 19:04:56 +0530

    docker demo

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,24 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hi/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi/i);
   expect(linkElement).toBeInTheDocument();
 });
